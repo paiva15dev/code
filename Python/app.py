6f2a043bf85fb1bb46b79bb4df42b5909c00d07b
paiva15dev <thiago.paiva.araujo@gmail.com>
2024-07-17 17:31:41 -0300

    Update app.py

--- Python/app.py
+++ Python/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask , request
 from models.task import Task
 app = Flask(__name__)
 
 tasks = []
 
-
-
+@app.route('/tasks', methods=['POSTS'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return 'Test'
 
 if __name__ == "__main__":
     app.run(debug=True)
